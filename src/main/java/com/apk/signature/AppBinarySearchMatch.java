94a513e3c86589c67348b914d7cc5e7ced1a53d1
sedej <mhatefi12@gmail.com>
2024-02-27 13:02:38 +0330

    change scan argument read flow

--- src/main/java/com/apk/signature/AppBinarySearchMatch.java
+++ src/main/java/com/apk/signature/AppBinarySearchMatch.java
@@ -1,6 +1,7 @@
 package com.apk.signature;
 
 import com.apk.signature.Model.MalwareModel;
+import com.apk.signature.Model.Report;
 import com.apk.signature.Model.ScanResult;
 import com.apk.signature.Model.SignatureModel;
 import com.apk.signature.Util.BinaryMatchCore;
@@ -11,55 +12,80 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static com.apk.signature.Util.Util.printGreen;
-import static com.apk.signature.Util.Util.printRed;
+import static com.apk.signature.Util.Util.*;
 
 public class AppBinarySearchMatch {
     public static void main(String[] args) {
-        String signature_path;
-        String target_path;
+        String signature_path = "";
+        String target_path = "";
         ArrayList<File> AllTargetFilePath = new ArrayList<>();
         BinaryMatchCore matchCore = new BinaryMatchCore();
-        if (args.length > 0 && args[0].equals(AppMain.SCAN)) {
-            signature_path = args[1];
-            target_path = args[2];
-            File file = new File(target_path);
-            if (file.getName().endsWith(".txt")) {
-                ArrayList<String> filesPath = new Util().readLineByLine(target_path);
-                for (String s : filesPath) {
-                    AllTargetFilePath.add(new File(s));
+        boolean arg_sig = false, arg_file = false, report = false;
+
+        if (args != null) {
+            int len = args.length;
+            if (len >= 2) {
+                signature_path = args[1];
+                arg_sig = !signature_path.isEmpty();
+                if (len >= 3) {
+                    report = args[2].equalsIgnoreCase("report");
+                    if (!report) {
+                        target_path = args[2];
+                        arg_file = !target_path.isEmpty();
+                    }
+                    if (len >= 4) {
+                        report = args[3].equalsIgnoreCase("report");
+                    }
                 }
-            } else {
-                AllTargetFilePath.add(file);
             }
         } else {
-            Scanner myObj = new Scanner(System.in);
+            AppBinarySearchMatch.main(new String[]{""});
+        }
+
+        Scanner myObj = new Scanner(System.in);
+        if (!arg_sig) {
             System.out.println("Enter signature file or folder");
             signature_path = myObj.nextLine();
-
+            if (signature_path.equalsIgnoreCase("exit")) System.exit(0);
+        }
+        if (!arg_file) {
             System.out.println("Enter target file or folder");
             target_path = myObj.nextLine();
+            if (target_path.equalsIgnoreCase("exit")) System.exit(0);
         }
 
         File fileSignature = new File(signature_path);
         if (!fileSignature.exists()) {
             printRed("signature path doesn't exist");
-            System.exit(0);
+            //System.exit(0);
+            AppBinarySearchMatch.main(new String[]{""});
         }
 
         File targetFile = new File(target_path);
         if (!targetFile.exists()) {
             printRed("target path doesn't exist");
-            System.exit(0);
+            //System.exit(0);
+            AppBinarySearchMatch.main(new String[]{"", signature_path});
+
+        }
+
+        File file = new File(target_path);
+        if (file.getName().endsWith(".txt")) {
+            ArrayList<String> filesPath = new Util().readLineByLine(target_path);
+            for (String s : filesPath) {
+                AllTargetFilePath.add(new File(s));
+            }
+        } else {
+            AllTargetFilePath.add(file);
         }
 
         Util util = new Util();
 
         ArrayList<SignatureModel> signatureModels = matchCore.getSigModels(fileSignature);
 
-        if (AllTargetFilePath.isEmpty()) {
+        /*if (AllTargetFilePath.isEmpty()) {
             AllTargetFilePath.add(targetFile);
-        }
+        }*/
 
         long start = System.currentTimeMillis();
         ArrayList<MalwareModel> malwareList = new ArrayList<>();
@@ -78,21 +104,22 @@ public class AppBinarySearchMatch {
         Gson gson = new Gson();
         ScanResult scanResult = new ScanResult();
 
-        scanResult.setTotalFiles(matchCore.getTotalFiles());
         scanResult.setTotalApk(matchCore.getTotalApk());
-        scanResult.setTotalSignature(signatureModels.size());
         scanResult.setTotalMalware(malwareList.size());
         scanResult.setTotalUnscannable(matchCore.getUnscannableModel().getSize());
         scanResult.setTotalTime(time);
 
-        String jsonSummarized = gson.toJson(scanResult);
+        String jsonSummarized = gson.toJson(new Report(scanResult));
 
-        scanResult.setMalwareList(malwareList);
-        scanResult.setUnscannable(matchCore.getUnscannableModel());
-
-        String json = gson.toJson(scanResult);
-        util.writeToFile(json, System.getProperty("user.dir") + "\\report-" + System.currentTimeMillis() + ".json");
+        if (report) {
+            scanResult.setTotalFiles(matchCore.getTotalFiles());
+            scanResult.setTotalSignature(signatureModels.size());
+            scanResult.setMalwareList(malwareList);
+            scanResult.setUnscannable(matchCore.getUnscannableModel());
+            String json = gson.toJson(scanResult);
+            util.writeToFile(json, System.getProperty("user.dir") + "\\report-" + System.currentTimeMillis() + ".json");
+        }
         printGreen(jsonSummarized);
-
+        AppBinarySearchMatch.main(new String[]{"", signature_path, "", report ? "report" : ""});
     }
 }
